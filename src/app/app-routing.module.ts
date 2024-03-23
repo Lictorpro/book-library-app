b49ab19e40a720908fc8ddc124e2c35bca2f38b0
David Mijić <dafi898@gmail.com>
2024-03-23 22:46:34 +0100

    Log in and register pages, new theme colors and auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
     path: 'books',
-    loadChildren: () => import('./books/books.module').then(m => m.BooksPageModule)
+    loadChildren: () => import('./books/books.module').then(m => m.BooksPageModule),
+    canLoad: [AuthGuard]
   },
   {
     path: '',
     redirectTo: 'books',
     pathMatch: 'full'
+  },
+  {
+    path: 'log-in',
+    loadChildren: () => import('./auth/log-in/log-in.module').then(m => m.LogInPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterPageModule)
   }
+
 
 ];
 
